788a23f33d060dc0ddca6f0d6d409c2ef7755197
Joris Valette <joris.valette@gmail.com>
2018-09-24 23:33:07 +0200

    Make the game a grid

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 extern crate ggez;
 
 use ggez::event::Keycode;
+use ggez::graphics::Color;
 use ggez::{event, graphics, Context, GameResult};
 
 use std::time::{Duration, Instant};
 
-
-const GRID_SIZE: (i32, i32) = (30, 20);
-const GRID_CELL_SIZE: (i32, i32) = (32, 32);
+const GRID_SIZE: (u32, u32) = (30, 20);
+const GRID_CELL_SIZE: (u32, u32) = (32, 32);
 
 const SCREEN_SIZE: (u32, u32) = (
-    GRID_SIZE.0 as u32 * GRID_CELL_SIZE.0 as u32,
-    GRID_SIZE.1 as u32 * GRID_CELL_SIZE.1 as u32,
+    GRID_SIZE.0 * GRID_CELL_SIZE.0,
+    GRID_SIZE.1 * GRID_CELL_SIZE.1,
 );
 
 const UPDATES_PER_SECOND: f32 = 8.0;
@@ -55,6 +55,15 @@ impl From<(i32, i32)> for GridPosition {
     }
 }
 
+impl From<(usize, usize)> for GridPosition {
+    fn from(pos: (usize, usize)) -> Self {
+        GridPosition {
+            x: pos.0 as i32,
+            y: pos.1 as i32,
+        }
+    }
+}
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum Direction {
     Up,
@@ -90,8 +99,32 @@ impl Cursor {
     }
 }
 
+struct Grid {
+    cells: [[Color; GRID_SIZE.1 as usize]; GRID_SIZE.0 as usize],
+}
+
+impl Grid {
+    pub fn new() -> Self {
+        Grid {
+            cells: [[graphics::WHITE; GRID_SIZE.1 as usize]; GRID_SIZE.0 as usize],
+        }
+    }
+
+    fn draw(&self, ctx: &mut Context) -> GameResult<()> {
+        for (row, row_item) in self.cells.iter().enumerate() {
+            for (col, col_item) in row_item.iter().enumerate() {
+                let grid_position: GridPosition = (row, col).into();
+                graphics::set_color(ctx, *col_item)?;
+                graphics::rectangle(ctx, graphics::DrawMode::Fill, grid_position.into())?;
+            }
+        }
+        Ok(())
+    }
+}
+
 struct GameState {
     cursor: Cursor,
+    grid: Grid,
     last_update: Instant,
 }
 
@@ -101,6 +134,7 @@ impl GameState {
 
         GameState {
             cursor: Cursor::new(cursor_pos),
+            grid: Grid::new(),
             last_update: Instant::now(),
         }
     }
@@ -117,6 +151,7 @@ impl event::EventHandler for GameState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx);
         self.cursor.draw(ctx)?;
+        self.grid.draw(ctx)?;
         graphics::present(ctx);
         ggez::timer::yield_now();
         Ok(())
@@ -139,9 +174,10 @@ fn main() {
     let ctx = &mut ggez::ContextBuilder::new("bigboard", "Joris Valette")
         .window_setup(ggez::conf::WindowSetup::default().title("Big Board"))
         .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1))
-        .build().expect("Failed to build ggez context");
+        .build()
+        .expect("Failed to build ggez context");
 
-    graphics::set_background_color(ctx, [1.0, 1.0, 1.0, 1.0].into());
+    graphics::set_background_color(ctx, [1.0, 0.41, 0.70, 1.0].into());
     let state = &mut GameState::new();
     match event::run(ctx, state) {
         Err(e) => println!("Error encountered running game: {}", e),
